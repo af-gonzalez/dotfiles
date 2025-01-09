b8d0d3130542d2f93fab329001812d1287071fe8
Andres Gonzalez <github@af-gonzalez.com>
2025-01-08 23:43:17 -0500

    feat: nvim indent guides

--- neovim/lua/plugins/tree-sitter.lua
+++ neovim/lua/plugins/tree-sitter.lua
@@ -2,4 +2,12 @@ return {
   {
     "nvim-treesitter/playground",
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = {
+        enable = true,
+      },
+    },
+  },
 }
